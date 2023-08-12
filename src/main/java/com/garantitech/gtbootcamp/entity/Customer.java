cef49b62aa19a99af302bb44c2829f1d89d91932
bahadirmemis <sadikbahadir.memis@migrosonline.com>
2023-08-12 10:23:17 +0300

    Add customer entity

--- src/main/java/com/garantitech/gtbootcamp/entity/Customer.java
+++ src/main/java/com/garantitech/gtbootcamp/entity/Customer.java
@@ -1,12 +1,19 @@
 package com.garantitech.gtbootcamp.entity;
 
+import com.garantitech.gtbootcamp.enums.EnumStatus;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import java.time.LocalDate;
+import java.util.Date;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -30,4 +37,26 @@ public class Customer {
   @Column(name = "surname", length = 50, nullable = false)
   private String surname;
 
+  @Column(name = "USERNAME", length = 20, nullable = false)
+  private String username;
+
+  @Column(name = "PASSWORD", length = 400, nullable = false)
+  private String password;
+
+  @Column(name = "EMAIL", length = 50, nullable = false)
+  private String email;
+
+  @Column(name = "PHONE_NUMBER", length = 15, nullable = false)
+  private String phoneNumber;
+
+  //@Temporal(TemporalType.DATE)
+  //@Column(name = "BIRTH_DATE1")
+  //private Date birthDate1;
+
+  @Column(name = "BIRTH_DATE")
+  private LocalDate birthDate;
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = "STATUS", length = 30)
+  private EnumStatus status;
 }
---
+++ src/main/java/com/garantitech/gtbootcamp/enums/EnumStatus.java
@@ -0,0 +1,10 @@
+package com.garantitech.gtbootcamp.enums;
+
+/**
+ * @author bahadirmemis
+ */
+public enum EnumStatus {
+
+  PASSIVE,
+  ACTIVE;
+}
